9b174b0e2641bbab570fe3e9744cd34c71eff6c1
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-03-02 19:06:24 +1100

    Add tests for evaluate

--- src/__tests__/robot/02-evaluate-input.test.js
+++ src/__tests__/robot/02-evaluate-input.test.js
@@ -2,6 +2,14 @@ const robot = require('../../../src/lib/robot')
 
 describe('Toy Robot Evaluate Input', () => {
   
+  beforeEach(() => {
+    jest.spyOn(console, 'log')
+  })
+
+  afterEach(() => {
+    console.log.mockRestore()
+  })
+
   it('recognizes that evaluate is a function', () => {
     const given = robot.evaluateCommands
     const expected = 'function'
@@ -10,9 +18,37 @@ describe('Toy Robot Evaluate Input', () => {
   })
 
   it('does not return anything', () => {
-    const given = ['PLACE X,Y,F', 'MOVE', 'LEFT', 'RIGHT', 'REPORT']
+    const given = ['PLACE 0,0,NORTH', 'MOVE', 'LEFT', 'RIGHT', 'REPORT']
+    const expected = undefined
     const result = robot.evaluateCommands(given)
     expect(result).toBeUndefined()
   })
 
+  it('evaluate commands ', () => {
+    const given = ['PLACE 0,0,NORTH', 'MOVE', 'LEFT', 'RIGHT', 'REPORT']
+    const expected = '0,1,NORTH'
+    const result = robot.evaluateCommands(given)
+    expect(console.log.mock.calls[0][0]).toEqual(expected)
+  })
+
+  it('evaluate commands', () => {
+    const given = ['PLACE 0,0,NORTH', 'MOVE', 'REPORT']
+    const expected = '0,1,NORTH'
+    const result = robot.evaluateCommands(given)
+    expect(console.log.mock.calls[0][0]).toEqual(expected)
+  })
+
+  it('evaluate commands', () => {
+    const given = ['PLACE 0,0,NORTH', 'LEFT', 'REPORT']
+    const expected = '0,0,WEST'
+    const result = robot.evaluateCommands(given)
+    expect(console.log.mock.calls[0][0]).toEqual(expected)
+  })
+
+  it('evaluate commands', () => {
+    const given = ['PLACE 1,2,EAST', 'MOVE', 'MOVE', 'LEFT', 'MOVE', 'REPORT']
+    const expected = '3,3,NORTH'
+    const result = robot.evaluateCommands(given)
+    expect(console.log.mock.calls[0][0]).toEqual(expected)
+  })
 })
