a1a653e934e0fd280d5d856cdce49b12065d255f
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-03-02 19:27:32 +1100

    Update the App to take more input files

--- src/app.js
+++ src/app.js
@@ -1,16 +1,29 @@
 const { readFileInput } = require('./lib/utils/file')
 const { evaluate } = require('./lib/robot')
 
-const file1 = process.cwd() + '/input/1.txt'
-
 const startApp = async () => {
   try {
-    const data = await readFileInput(file1)
-    evaluate(data)
-  }
-  catch(error) {
+    // Input files
+    const file1 = process.cwd() + '/input/1.txt'
+    const file2 = process.cwd() + '/input/2.txt'
+    const file3 = process.cwd() + '/input/3.txt'
+    const file4 = process.cwd() + '/input/4.txt'
+
+    // Read file input
+    const data1 = await readFileInput(file1)
+    const data2 = await readFileInput(file2)
+    const data3 = await readFileInput(file3)
+    const data4 = await readFileInput(file4)
+
+    // Evaluate data from each file
+    evaluate(data1)
+    evaluate(data2)
+    evaluate(data3)
+    evaluate(data4)
+  } catch (error) {
     console.log(error)
   }
 }
 
+// Run the App
 startApp()
